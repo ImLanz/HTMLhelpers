726b87256194e2eee2eb34a69136f01bfb4de9c0
Lanz S. Rodz <37322363+LanzFlash@users.noreply.github.com>
2019-03-29 20:23:10 -0400

    Lance
    HTML HELPERS, Formulario

---
+++ HELPERS/Models/Formulario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HTMLhelpers.Models
+{
+    public class Formulario
+    {
+
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String cedula { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String apellido { get; set; }
+
+        [Range(15,100,ErrorMessage ="Debes ser mayor de 15 años")]
+        [Required(ErrorMessage = "Campo Requerido")]
+        public int edad { get; set; }
+
+        [MinLength(10)]
+        public String telefono { get; set; }
+
+        [EmailAddress(ErrorMessage ="Correo no encontrado")]
+        [RegularExpression("^[_A-Za-z'`+-.]+([_A-Za-z0-9'+-.]+)*@([A-Za-z0-9-])+(\\.[A-Za-z0-9]+)*(\\.([A-Za-z]*){3,})$", ErrorMessage = "Formato incorrecto")]
+        [Required(ErrorMessage ="El correo es requerido")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Formato Incorrecto")]
+        public String Correo { get; set; }
+
+        public String genero { get; set; }
+
+        public String estado { get; set; }
+
+        public string[] hobbies { get; set; }
+
+        public generos GenerosPersonas { get; set; }
+
+    }
+
+    public enum generos
+    {
+        Masculino,
+        Femenino,
+        Otro
+    }
+}
---
+++ HELPERS/Models/FormularioContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HTMLhelpers.Models
+{
+    public class FormularioContext:DbContext
+    {   
+        public FormularioContext()
+            : base("FormularioData")
+        { 
+        }
+
+        public DbSet<Formulario> formulario { get; set; }
+    }
+}
